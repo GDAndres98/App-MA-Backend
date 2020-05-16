6f44996641665e5b6968b4420e8565f646ad58ba
Andrés Osorio <gd_andres98@hotmail.com>
2020-05-16 05:36:35 -0500

    Creación tabla Article

--- src/main/java/app/ma/entities/Article.java
+++ src/main/java/app/ma/entities/Article.java
@@ -7,14 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 @Table(name = "article")
@@ -27,15 +23,14 @@ public class Article {
 	@Column(nullable = false)
 	private String title;
 	@Column(nullable = false)
-	private String description;
+	private String author;
+	@Column(nullable = false)
+	private String markdownURL;
+	@Column(nullable = false)
+	private Date dateWritten;
 
 	// TODO Tag-Article
 	// TODO Level ID
-
-	@OneToOne
-	@JoinColumn(name = "posted_at", nullable = false)
-	@JsonManagedReference
-	private Class postedAt;
 
 	@CreationTimestamp
 	private Date createAt;
@@ -46,6 +41,52 @@ public class Article {
 
 	// -------------------------------------------------------------------
 
+	public Long getId() {
+		return id;
+	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getMarkdownURL() {
+		return markdownURL;
+	}
+
+	public void setMarkdownURL(String markdownURL) {
+		this.markdownURL = markdownURL;
+	}
+
+	public Date getDateWritten() {
+		return dateWritten;
+	}
+
+	public void setDateWritten(Date dateWritten) {
+		this.dateWritten = dateWritten;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public Date getUpdateAt() {
+		return updateAt;
+	}
 
 }
