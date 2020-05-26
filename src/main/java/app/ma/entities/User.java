27fe4d67515e25968167b8f7b218853d759fdddb
Marlon <maestupinan2@poligran.edu.co>
2020-05-26 12:33:25 -0500

    No lo se xd

--- src/main/java/app/ma/entities/User.java
+++ src/main/java/app/ma/entities/User.java
@@ -19,6 +19,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 @Entity
--- src/main/java/app/ma/repositories/PostRepository.java
+++ src/main/java/app/ma/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package app.ma.repositories;
 import java.util.List;
 import java.util.Set;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,6 @@ import app.ma.entities.*;
 
 @Repository
 public interface PostRepository extends CrudRepository<Post, Long> {
+	
 	public List<Post> findByUserCourse_Course_IdOrderByCreationDateAsc(Long id);
 }
